3f8aff0037eb5650fe660212ba38805b7954db56
dibyayan97 <34715957+dibyayan97@users.noreply.github.com>
2017-12-20 22:16:15 +0530

    Update Dictator.py

--- Dictator.py
+++ Dictator.py
@@ -1,3 +1,7 @@
+#The idea of this code is build a football playing bot that can avoid all the 'player' bots and reach the ball and then shoot it to the goal.#
+#So, image processing using opencCV was employed to detect the ball, all obstacles, goal and player bot#
+#Once this data is obtained, we run the algorithm and determine the trajecotory the bot has to take#
+
 import Field
 import Imagepart
 
